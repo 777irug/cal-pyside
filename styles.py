af553da3f41b0892524bb956ef224bee049bb3e8
777irug <67226582+777irug@users.noreply.github.com>
2021-06-03 14:13:53 +0530

    Add files via upload

---
+++ styles.py
@@ -0,0 +1,127 @@
+
+class style():
+
+    # MAIN WINDOW
+    mainWindow = ("""QToolTip {
+                   background: rgb(50, 50, 50);
+                   color: rgb(200, 200, 200);
+                   border: 1px solid rgb(40, 40, 40);
+                   }
+                   QLabel { color: rgb(200, 200, 200); }""")
+
+    bts_title_bar_minimize = (u"QPushButton {\n"
+    "	background-color: transparent; \n"
+    "	background-image: url(:/PNGs/Images/cil-window-minimize.png);\n"
+    "	background-position: center;\n"
+    "	background-repeat: no-repeat;\n"
+    "	border-radius: 10px;\n"
+    "}\n"
+    "QPushButton:hover {\n"
+    "	background-color: rgb(45, 45, 45); 	\n"
+    "}\n"
+    "QPushButton:pressed {\n"
+    "	background-color: rgb(25, 25, 25); \n"
+    "	border: 2px solid rgb(255, 0, 127);\n"
+    "}")
+
+
+    bts_title_bar_maximize = (u"QPushButton {\n"
+    "	background-color: transparent; \n"
+    "	background-image: url(:/PNGs/Images/cil-window-maximize.png);\n"
+    "	background-position: center;\n"
+    "	background-repeat: no-repeat;\n"
+    "	border-radius: 10px;\n"
+    "}\n"
+    "QPushButton:hover {\n"
+    "	background-color: rgb(45, 45, 45); 	\n"
+    "}\n"
+    "QPushButton:pressed {\n"
+    "	background-color: rgb(25, 25, 25); \n"
+    "	border: 2px solid rgb(255, 0, 127);\n"
+    "}")
+
+    bts_title_bar_restore = (u"QPushButton {\n"
+    "	background-color: transparent; \n"
+    "	background-image: url(:/PNGs/Images/cil-window-restore.png);\n"
+    "	background-position: center;\n"
+    "	background-repeat: no-repeat;\n"
+    "	border-radius: 10px;\n"
+    "}\n"
+    "QPushButton:hover {\n"
+    "	background-color: rgb(45, 45, 45); 	\n"
+    "}\n"
+    "QPushButton:pressed {\n"
+    "	background-color: rgb(25, 25, 25); \n"
+    "	border: 2px solid rgb(255, 0, 127);\n"
+    "}")
+
+    bts_title_bar_close = (u"QPushButton {\n"
+    "	background-color: transparent; \n"
+    "	background-image: url(:/PNGs/Images/cil-x.png);\n"
+    "	background-position: center;\n"
+    "	background-repeat: no-repeat;\n"
+    "	border-radius: 10px;\n"
+    "}\n"
+    "QPushButton:hover {\n"
+    "	background-color: rgb(45, 45, 45); 	\n"
+    "}\n"
+    "QPushButton:pressed {\n"
+    "	background-color: rgb(25, 25, 25); \n"
+    "	border: 2px solid rgb(255, 0, 127);\n"
+    "}")
+
+    bts_numbers = (u"QPushButton {\n"
+    "	background-color: rgba(45, 45, 45, 50); \n"
+    "	color: rgb(200, 200, 200);\n"
+    "	border-radius: 10px;\n"
+    "}\n"
+    "QPushButton:hover {\n"
+    "	background-color: rgba(255, 0, 127, 100); \n"
+    "}\n"
+    "QPushButton:pressed {\n"
+    "	background-color: rgba(255, 0, 127, 150); \n"
+    "	border: 2px solid rgb(255, 0, 127);\n"
+    "}")
+
+
+    bts_functions = (u"QPushButton {\n"
+    "	background-color: rgba(35, 35, 35, 15); \n"
+    "	color: rgb(200, 200, 200);\n"
+    "	border-radius: 10px;\n"
+    "}\n"
+    "QPushButton:hover {\n"
+    "	background-color: rgba(255, 0, 127, 50); \n"
+    "}\n"
+    "QPushButton:pressed {\n"
+    "	background-color: rgba(255, 0, 127, 100); \n"
+    "	border: 2px solid rgb(255, 0, 127);\n"
+    "}")
+
+    bts_backspace = (u"QPushButton {\n"
+    "	background-color: rgba(35, 35, 35, 15);	\n"
+    "	background-image: url(:/PNGs/Images/backspace.png);\n"
+    "	background-position: center;\n"
+    "	background-repeat: no-repeat;\n"
+    "	color: rgb(200, 200, 200);\n"
+    "	border-radius: 10px;\n"
+    "}\n"
+    "QPushButton:hover {\n"
+    "	background-color: rgba(255, 0, 127, 50); \n"
+    "}\n"
+    "QPushButton:pressed {\n"
+    "	background-color: rgba(255, 0, 127, 100); \n"
+    "	border: 2px solid rgb(255, 0, 127);\n"
+    "}")
+
+    bts_equal = (u"QPushButton {\n"
+    "	background-color: rgba(35, 35, 35, 15); \n"
+    "	color: rgb(200, 200, 200);\n"
+    "	border-radius: 10px;\n"
+    "}\n"
+    "QPushButton:hover {\n"
+    "	background-color: rgba(255, 0, 127, 100); \n"
+    "}\n"
+    "QPushButton:pressed {\n"
+    "	background-color: rgba(255, 0, 127, 150); \n"
+    "	border: 2px solid rgb(255, 0, 127);\n"
+    "}")
